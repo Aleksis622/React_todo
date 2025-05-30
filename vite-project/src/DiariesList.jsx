d68cc0dc2fd8bb36216cf9035d2b01f6e868f0b6
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-05-30 12:22:57 +0300

    diarieslist ir pabeigts!

--- vite-project/src/DiariesList.jsx
+++ vite-project/src/DiariesList.jsx
@@ -8,23 +8,23 @@ const [Diaries] = useState([
       title:"Aiziet ar draugiem nopeldēties",
       body: "Biju jūrā līdz 15:00",
       date:"2025.04.06" },
- {
-  id:2,
-  title:"Paēst",
-  body:"Hesburgerā paēdu",
-  date:"2025.04.06"
+    {
+      id:2,
+      title:"Paēst",
+      body:"Hesburgerā paēdu",
+      date:"2025.04.06"
 },
-{ 
- id:3,
-title:"Atpūsties",
-body:"Skatījos TV",
-date:"2025.04.06"
+   { 
+     id:3,
+     title:"Atpūsties",
+     body:"Skatījos TV",
+     date:"2025.04.06"
 },
-{
-id:4,
-title:"Aiziet laicīgi gulēt",
-body:"Ievēlos gultā 2:00",
-date:"2025.04.06"
+  {
+     id:4,
+     title:"Aiziet laicīgi gulēt",
+     body:"Ievēlos gultā 2:00",
+     date:"2025.04.06"
 }    
   ]);
   return (
